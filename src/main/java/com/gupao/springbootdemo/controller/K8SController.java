5e0fd128e423425268ad05f3f7574835fa7c1ed4
liuzc <zongchang.liu@zhiyoubao.com>
2020-05-08 22:27:57 +0800

    修改返回值试一下

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -11,6 +11,6 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
-        return "hello K8s <br/>111222 坎坷中前行，也会达到目标";
+        return "hello K8s <br/>111222 坎坷中前行，也会达到目标, 真是坎坷呢";
     }
 }
